01d287d5534c9e1a5c152ef5721093a7641875f1
David EGEA <contactdavidegea@gmail.com>
2023-04-27 18:46:21 +0200

    ✅ main: MaJ File

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def not_found(error):
 if __name__ == '__main__':
     with app.app_context():
         port = int(os.environ.get('PORT', 5000))
-        app.run(debug=True)
+        app.run(host='0.0.0.0', port=port)
