eb30e6986261deabd7020fb6f4683b503f593bbc
David EGEA <contactdavidegea@gmail.com>
2023-05-02 08:50:03 +0200

    ✅ main : MaJ App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Catalogue from "./containers/Catalogue/Catalogue";
 import Admin from "./containers/Admin/Admin";
 import PrivateRoute from "./components/Shared/PrivateRoute";
 import Error from "./components/Error/Error";
-import LoginPage from "./containers/Auth/LoginPage";
+import Login from "./containers/Auth/Login";
 import Connect from "./containers/Admin/Connect";
 import ProductForm from "./components/Form/ProductForm";
 import CategoryForm from "./components/Form/CategoryForm";
@@ -23,12 +23,11 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/catalogue" element={<Catalogue />} />
               <Route path="/dashboard" element={<PrivateRoute><Connect /></PrivateRoute>} />
-              {/* <Route path="/login" element={<Login />} /> */}
+              <Route path="/login" element={<Login />} />
               <Route path="/admin" element={<Admin />} exact />
               <Route path="/admin/products" element={<ProductForm />} exact />
               <Route path="/admin/categories" element={<CategoryForm />} exact />
               <Route path="/admin/promotions" element={<PromotionForm />} exact />
-              <Route path="/loginpage" element={<LoginPage />} />
               <Route path="*" element={<Error message="404 - Page not found" />} />
             </Routes>
           </Layout> 
