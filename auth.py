d934856783739092a349bbc78601e8272a5c92a0
David EGEA <contactdavidegea@gmail.com>
2023-05-02 09:48:11 +0200

    ✅ main: MaJ Auth

--- auth.py
+++ auth.py
@@ -2,9 +2,8 @@ import logging
 import os
 from uuid import uuid4
 
-from database import validate_test
-from flask import (Blueprint, jsonify, make_response, redirect, request,
-                   session, url_for)
+from database import compare_hashed_passwords, validate_user
+from flask import (Blueprint, jsonify, make_response, redirect, request, session, url_for)
 
 # Configure le logger pour enregistrer les messages dans le fichier logs.txt
 logs_dir = os.path.join(os.path.dirname(__file__), 'logs')
@@ -21,12 +20,12 @@ auth_bp = Blueprint('auth', __name__)
 
 auth_bp.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@auth_bp.route('/auth/loginpage', methods=['POST'])
+@auth_bp.route('/auth/login', methods=['POST'])
 def validate_login():
     if request.method == 'POST':
         username = request.json['username']
         password = request.json['password']
-        user = validate_test(username, password)
+        user = validate_user(username, password)
         logger.info(f"Utilisateur trouvé : {user}")
         if user is not None:
             # L'utilisateur est valide, redirige vers la page d'administration
@@ -36,46 +35,17 @@ def validate_login():
             session['session_id'] = str(uuid4())
             response = make_response('Success')
             response.set_cookie('session_id', session['session_id'], secure=True, httponly=True, samesite='Strict')
-            return 'Success'
-
+            return response
         else:
             # L'utilisateur ou le mot de passe est incorrect, affiche un message d'erreur et redirige vers la page de connexion
-            logger.info('Mot de passe invalide (app.py)')
-            response = make_response(jsonify({'message': 'les informations d\'identification invalides (app.py)'}), 401)
-            response.set_cookie('session_id', '', expires=0)
-            return jsonify({'message': 'les informations d\'identification invalides (app.py)'}), 401
+            logger.info('Nom d\'utilisateur ou mot de passe invalide (app.py)')
+            response = make_response(jsonify({'message': 'Nom d\'utilisateur ou mot de passe invalide (app.py)'}), 401)
+            response.set_cookie('session_id', '', expires=0, secure=True, httponly=True, samesite='Strict')
+            return response
+
 
 @auth_bp.route('/auth/logout')
 def logout():
     session.pop('logged_in', None)
     session.pop('username', None)
-    return redirect(url_for('auth.validate_loginpage'))
-
-
-# @auth_bp.route('/auth/login', methods=['POST'])
-# def login():
-#     data = request.json
-#     username = data['username']
-#     password = data['password']
-    
-#     logger.debug(f'username (login view) (auth.py): {username}') 
-
-#     user = validate_user(username, password)
-
-#     logger.debug(f'SELECT * FROM admin WHERE username = "{username}"')
-
-#     if user is None:
-#         logger.debug('Nom d\'utilisateur invalide (auth.py)')
-#         return jsonify({'message': 'les informations d\'identification invalides (auth.py)'}), 401
-
-#     logger.debug(f'utilisateur (auth.py) 1 : {username}')
-#     logger.debug(f'mot de passe (auth.py) 2 : {password}')
-#     logger.debug(f'utilisateur (auth.py) 3 : {user}')
-#     logger.debug(f'password_hash (auth.py) 4 : {user["password"]}')
-    
-#     if compare_hashed_passwords(user['password'], password, user['salt']):
-#         logger.debug('Connexion avec succes (auth.py)')
-#         return jsonify({'message': 'Connexion avec succes (auth.py)'}), 200
-#     else:
-#         logger.debug('Mot de passe invalide (auth.py)')
-#         return jsonify({'message': 'les informations d\'identification invalides (auth.py)'}), 401
+    return redirect(url_for('auth.validate_login'))
